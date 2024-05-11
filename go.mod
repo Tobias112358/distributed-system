d19631fb9487ea6e52118881ca329693787db7a3
Toby Loveridge <tobias.loveridge@gmail.com>
2024-05-11 17:13:51 +1000

    added in code from tutorial.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/Tobias112358/distributed-system
 
 go 1.22.1
+
+require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/golang/protobuf v1.5.4
+	google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf v1.34.1
+)
--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/Tobias112358/distributed-system/core"
+)
 
 func main() {
-
-	fmt.Println("Hello World")
+	nodeType := os.Args[0]
+	println("*****")
+	println(nodeType)
+	println("*****")
+	switch nodeType {
+	case "master":
+		core.GetMasterNode().Start()
+	case "worker":
+		core.GetWorkerNode().Start()
+	default:
+		panic("invalid node type")
+	}
 }
